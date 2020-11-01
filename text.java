7e98df93a02b3b0633ba21f4b114daa4ba686b71
TXQX <72741105+TXQX@users.noreply.github.com>
2020-11-02 00:34:50 +0800

    Add files via upload

---
+++ text.java
@@ -0,0 +1,11 @@
+package choose;
+
+public class text{
+	public static void main(String choose[]){
+		student stu1 = new student("1","小泽","男","数学");
+		teacher tea1 = new teacher("1","小红","女","美术");
+		System.out.println("1","小泽","男","数学");
+		System.out.println("1","小红","女","美术");
+	}
+}
+
